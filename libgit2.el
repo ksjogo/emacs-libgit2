ec3c3925a78b5b7246ba61fb654d38e5f0f1bbbe
Mario Rodas <marsam@users.noreply.github.com>
2016-08-22 18:02:08 -0500

    Add libgit2.el package headers

--- libgit2.el
+++ libgit2.el
@@ -1,7 +1,21 @@
-(require 'libgit2-core nil t)
+;;; libgit2.el --- Emacs bindings for libgit2         -*- lexical-binding: t -*-
+
+;; Copyright (c) 2016 Johannes Bernhard Goslar
+
+;; Author: Johannes Bernhard Goslar
+;; URL:
+;; Keywords: git tools vc
+;; Version: 0.1
+;; Package-Requires: ((emacs "25"))
+
+;;; Commentary:
+
+;;; Code:
+(require 'libgit2-core)
 
 (defun libgit2-status ()
   (interactive)
   (libgit2-core-status (expand-file-name ".")))
 
 (provide 'libgit2)
+;;; libgit2.el ends here
