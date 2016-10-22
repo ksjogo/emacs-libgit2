a8b39812e1f0cd4d7b10d4506425d00417d58039
Johannes 'ksjogo' Goslar <jogo@kronberger-spiele.de>
2016-10-22 02:39:36 +0200

    c coverage

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,9 +1,13 @@
 (add-to-list 'load-path default-directory)
 
+(defun libgit2-gcov-collector ()
+  (libgit2-core-dump-gcov)
+  (undercover-gcov-collector (file-expand-wildcards "*.gcov")))
+
 (when (require 'undercover nil t)
+  (add-to-list 'undercover-additional-files #'libgit2-gcov-collector)
   (if (getenv "TRAVIS")
       (undercover "*.el")
-    (message "undercover")
     (setq undercover-force-coverage t)
     (setenv "COVERALLS_REPO_TOKEN" "localrun")
     (undercover "*.el" (:report-file (expand-file-name "./coverage.json")) (:send-report nil))))
